832c2fea7a398ede27736ceea55d5377908625ca
25077667 <zxc25077667@gmail.com>
2020-02-28 23:50:15 +0800

    Get size shorter

--- queue.c
+++ queue.c
@@ -123,9 +123,7 @@ bool q_remove_head(queue_t *q, char *sp, size_t bufsize)
  */
 int q_size(queue_t *q)
 {
-    if (q == NULL)
-        return 0;
-    return q->size;
+    return q ? q->size : 0;
 }
 
 /*
